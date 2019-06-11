c972fd2e37b6be081a33c4721413af500885cd12
tjohnny93 <tjohnny93@berkeley.edu>
2019-06-11 16:24:35 -0700

    Redirect user to default sub index page when user already logged in and still tries to access login page

--- reddit_clone/app/controllers/sessions_controller.rb
+++ reddit_clone/app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
 
   def new
-
+    redirect_to subs_url if logged_in?
   end
 
   def create
     user = User.find_by_credentials(params[:user][:username], params[:user][:password])
     if user
       login(user)
-      # redirect_to 
+      redirect_to subs_url 
     else
       flash.now[:errors] = ["Invalid Credentials"]
       render :new
