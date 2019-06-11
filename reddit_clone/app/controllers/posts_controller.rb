d14d7f4ec992f4ae93cedfb881057b37a95bb90e
tjohnny93 <tjohnny93@berkeley.edu>
2019-06-11 11:51:06 -0700

    Add methods to Subs controller

--- reddit_clone/app/controllers/posts_controller.rb
+++ reddit_clone/app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+  
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+  
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :url, :content)
+  end
+
 end
--- reddit_clone/app/controllers/subs_controller.rb
+++ reddit_clone/app/controllers/subs_controller.rb
@@ -1,2 +1,45 @@
 class SubsController < ApplicationController
+
+  def index
+    @subs = Sub.all
+  end
+
+  def show
+    @sub = Sub.find_by(id: params[:id])
+  end
+
+  def new
+    @sub = Sub.new
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+    @sub.user_id = current_user.id
+    if @sub.save
+      redirect_to subs_url
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @sub = Sub.find_by(id: params[:id])
+  end
+
+  def update
+    @sub = current_user.subs.find(params[:id])
+    if @sub.update_attributes(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :edit
+    end
+  end
+
+  def sub_params
+    params.require(:sub).permit(:title, :description)
+  end
+
+
 end
